3092815a4adc918f121bdad5c83ca7299d6d482f
Merlin <github@bothimtv.com>
2024-08-26 12:36:22 +0200

    Aufgabe 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public static void main (String[] args) {
 
     // Aufgabe 1
@@ -13,5 +15,31 @@ public static void main (String[] args) {
 
     String name = "Merlin";
     System.out.println("Hallo, " + name);
+
+    // Aufgabe 3
+    // 3.1
+
+    Scanner scanner = new Scanner(System.in);
+    int res = scanner.nextInt();
+
+    if (res > 10) System.out.println("Zahl größer als 10!");
+    if (res < 10) System.out.println("Zahl kleiner oder gleich 10!");
+
+    // 3.2
+
+    /**
+     * Bei 25, "Es ist warm."
+     * Bei 10, "Es ist kühl."
+     * Bei -20, "Es ist eisig kalt!"
+     */
+
+    int temperatur = 25;
+    if (temperatur < 0) {
+        System.out.println("Es ist eigig kalt!");
+    } else if (temperatur < 15) {
+        System.out.println("Es ist kühl.");
+    } else {
+        System.out.println("Es ist warm.");
+    }
 
 }
