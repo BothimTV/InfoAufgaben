701358689c5aefe64d05d5c2d474bcd73138a415
Merlin <github@bothimtv.com>
2024-08-26 12:30:24 +0200

    Aufgabe 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,15 @@ public static void main (String[] args) {
     // Aufgabe 1
     System.out.println("Hello world!");
 
+    // Aufgabe 2
+    // 2.1
+
+    int alter = 18;
+    System.out.println("Alter: " + alter);
+
+    // 2.2
+
+    String name = "Merlin";
+    System.out.println("Hallo, " + name);
+
 }
