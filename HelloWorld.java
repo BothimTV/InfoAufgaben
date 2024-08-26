c277b61a8fc8e285a538cc4022a4beaeac7380d5
Merlin <github@bothimtv.com>
2024-08-26 11:13:08 +0000

    Formatierung & Aufgabe 5 Ausgabe

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,82 +6,82 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main (String[] args) {
 
-    // Aufgabe 1
-    System.out.println("Hello world!");
+        // Aufgabe 1
+        System.out.println("Hello world!");
 
-    // Aufgabe 2
-    // 2.1
+        // Aufgabe 2
+        // 2.1
 
-    int alter = 18;
-    System.out.println("Alter: " + alter);
+        int alter = 18;
+        System.out.println("Alter: " + alter);
 
-    // 2.2
+        // 2.2
 
-    String name = "Merlin";
-    System.out.println("Hallo, " + name);
+        String name = "Merlin";
+        System.out.println("Hallo, " + name);
 
-    // Aufgabe 3
-    // 3.1
+        // Aufgabe 3
+        // 3.1
 
-    Scanner scanner = new Scanner(System.in);
-    int res = scanner.nextInt();
-    scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        int res = scanner.nextInt();
+        scanner.close();
 
-    if (res > 10) System.out.println("Zahl größer als 10!");
-    if (res < 10) System.out.println("Zahl kleiner oder gleich 10!");
+        if (res > 10) System.out.println("Zahl größer als 10!");
+        if (res < 10) System.out.println("Zahl kleiner oder gleich 10!");
 
-    // 3.2
+        // 3.2
 
-    /**
-     * Bei 25, "Es ist warm."
-     * Bei 10, "Es ist kühl."
-     * Bei -20, "Es ist eisig kalt!"
-     */
+        /**
+         * Bei 25, "Es ist warm."
+         * Bei 10, "Es ist kühl."
+         * Bei -20, "Es ist eisig kalt!"
+         */
 
-    int temperatur = 25;
-    if (temperatur < 0) {
-        System.out.println("Es ist eigig kalt!");
-    } else if (temperatur < 15) {
-        System.out.println("Es ist kühl.");
-    } else {
-        System.out.println("Es ist warm.");
+        int temperatur = 25;
+        if (temperatur < 0) {
+            System.out.println("Es ist eigig kalt!");
+        } else if (temperatur < 15) {
+            System.out.println("Es ist kühl.");
+        } else {
+            System.out.println("Es ist warm.");
+        }
+
+        // Aufgabe 4
+        // 4.1
+        int i = 0;
+        while (i < 10) {
+            i++;
+        }
+
+        // 4.2
+        for (int x = 10; x <= 1; x--) {
+            System.out.println(x);
+        }
+
+        // 4.3
+
+        /**
+         * Ausgabe:
+         * "*****"
+         */
+        // Vor Änderung
+        for (int y = 0; y < 5; y++) {
+            System.out.println("*");
+        }
+
+        // Nach Änderung
+        for (int z = 0; z < 1; z++) {
+            System.out.println("**\n");
+        }
+
+        // Aufgabe 5 (Zusatz)
+        int n = 10;
+        int produkt = 1;
+        while(n > 0) {
+            produkt *= n;
+            n--;
+        }
+        System.out.println(produkt);
     }
-
-    // Aufgabe 4
-    // 4.1
-    int i = 0;
-    while (i < 10) {
-        i++;
-    }
-
-    // 4.2
-    for (int x = 10; x <= 1; x--) {
-        System.out.println(x);
-    }
-
-    // 4.3
-
-    /**
-     * Ausgabe:
-     * "*****"
-     */
-    // Vor Änderung
-    for (int y = 0; y < 5; y++) {
-        System.out.println("*");
-    }
-
-    // Nach Änderung
-    for (int z = 0; z < 1; z++) {
-        System.out.println("**\n");
-    }
-
-    // Aufgabe 5 (Zusatz)
-    int n = 10;
-    int produkt = 1;
-    while(n > 0) {
-        produkt *= n;
-        n--;
-    }
-
-}
 }
