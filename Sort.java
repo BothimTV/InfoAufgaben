5c76c7c6221e8830865188878b71dfdbe09d55ef
Merlin <github@bothimtv.com>
2024-10-21 11:06:36 +0000

    Fix Aufgabe 1 :)

--- Sort.java
+++ Sort.java
@@ -1,20 +1,25 @@
 public class Sort {
-    public static void main(args String[]) {
+    public static void main(String[] args) {
 
-        int[] arr = { 4; 39; 4; 0; 21; 8; 2 }
+        int[] arr = { 4, 39, 4, 0, 21, 8, 2 };
         boolean läuft = true;
 
         while (läuft) {
             boolean änderung = false;
             for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i+1]) {
+                if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                     änderung = true;
                 }
             }
-            if (!änderung) läuft = false;
+            if (!änderung)
+                läuft = false;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
 
     }
