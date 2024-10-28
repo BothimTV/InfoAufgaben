539a9a06a3613549fc62024580b78ab5a50dfc92
Merlin <github@bothimtv.com>
2024-10-28 12:09:09 +0000

    Effizienz +1

--- Sortieren.java
+++ Sortieren.java
@@ -33,7 +33,7 @@ public class Sortieren {
     void selectionSort() {
         for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
