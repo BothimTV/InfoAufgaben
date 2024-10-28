5ee884f9f2ac4d35f70d7b6675ef6775f54d572e
Merlin <github@bothimtv.com>
2024-10-28 11:47:52 +0000

    Verschoben: SelectionSort

--- Sortieren.java
+++ Sortieren.java
@@ -31,8 +31,21 @@ public class Sortieren {
      * Selection Sort (Sortieren durch Auswahl)
      */
     void selectionSort() {
-        // hier implementieren
-        // ...
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     /*
