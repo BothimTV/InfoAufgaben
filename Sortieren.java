2842c805f1c8b4a6095f9ef6d3079a010b8a43ee
Merlin <github@bothimtv.com>
2024-10-28 11:47:17 +0000

    Verschoben: InsertionSort

--- Sortieren.java
+++ Sortieren.java
@@ -67,7 +67,17 @@ public class Sortieren {
      * Insertion Sort (Sortieren durch Einf�gen)
      */
     void insertionSort() {
-        // hier implementieren
-        // ...
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
