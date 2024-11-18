15efd7846efcfc20b8244458b129974b5f145ef0
Merlin <github@bothimtv.com>
2024-11-18 11:55:16 +0100

    Add files via upload

---
+++ Bahnhof.java
@@ -0,0 +1,78 @@
+public class Bahnhof{
+  private StackString gleis1;
+  private StackString gleis2;
+  private StackString gleis3;
+  
+  
+  public Bahnhof() {
+    gleis1 = new StackString();
+    gleis2 = new StackString();
+    gleis3 = new StackString();
+  }
+  
+  public void fuellen(String farbe){
+    gleis1.push(farbe);
+  }
+  
+  public void sortieren() {
+    // Solange Gleis 1 nicht leer ist, werden Waggons sortiert.
+    while (!gleis1.isEmpty()) {
+        // Den obersten Waggon von Gleis 1 nehmen und zwischenspeichern.
+        String temp = gleis1.pop();
+
+        // Wenn der Waggon rot ist, wird er auf Gleis 2 geschoben.
+        if (temp.equals("rot")) {
+            gleis2.push(temp);
+        }
+        // Wenn der Waggon blau ist, wird er auf Gleis 3 geschoben.
+        if (temp.equals("blau")) {
+            gleis3.push(temp);
+        }
+    } // Ende der while-Schleife
+}
+  
+  public void gleiseAusgeben() {
+
+    // Gleis 1 ausgeben
+    System.out.println("Gleis 1: ");
+    // Temporären Stack erstellen, um die Waggons von Gleis 1 zwischenzuspeichern
+    StackString temp1 = new StackString();
+    // Alle Waggons von Gleis 1 in den temporären Stack verschieben und ausgeben
+    while (!gleis1.isEmpty()) {
+        String waggon = gleis1.pop(); // Waggon vom Gleis nehmen
+        System.out.println(waggon);   // Waggon ausgeben
+        temp1.push(waggon);        // Waggon in den temporären Stack legen
+    }
+    // Waggons vom temporären Stack zurück auf Gleis 1 verschieben
+    while (!temp1.isEmpty()) {
+        gleis1.push(temp1.pop()); // Waggon vom temporären Stack zurück auf Gleis 1 legen
+    }
+
+    // Gleis 2 ausgeben (analog zu Gleis 1)
+    System.out.println("Gleis 2: ");
+    StackString temp2 = new StackString();
+    while (!gleis2.isEmpty()) {
+        String waggon = gleis2.pop();
+        System.out.println(waggon);
+        temp2.push(waggon);
+    }
+    while (!temp2.isEmpty()) {
+        gleis2.push(temp2.pop());
+    }
+
+    // Gleis 3 ausgeben (analog zu Gleis 1 und 2)
+    System.out.println("Gleis 3: ");
+    StackString temp3 = new StackString();
+    while (!gleis3.isEmpty()) {
+        String waggon = gleis3.pop();
+        System.out.println(waggon);
+        temp3.push(waggon);
+    }
+    while (!temp3.isEmpty()) {
+        gleis3.push(temp3.pop());
+    }
+
+    System.out.println(" "); // Leerzeile zur besseren Lesbarkeit
+}
+  
+}
---
+++ BahnhofTest.java
@@ -0,0 +1,68 @@
+public class BahnhofTest {
+
+    public static void main(String[] args) {
+
+        // Test 1: Erstellen eines neuen Bahnhofs
+        System.out.println("Test 1: Erstellen eines neuen Bahnhofs");
+        Bahnhof bahnhof = new Bahnhof();
+        System.out.println("Bahnhof erstellt.");
+
+        // Test 2: Befüllen von Gleis 1 mit Waggons
+        System.out.println("\nTest 2: Befüllen von Gleis 1 mit Waggons");
+        bahnhof.fuellen("rot");
+        bahnhof.fuellen("blau");
+        bahnhof.fuellen("rot");
+        bahnhof.fuellen("rot");
+        bahnhof.fuellen("blau");
+        System.out.println("Gleis 1 befüllt mit: rot, blau, rot, rot, blau");
+
+        // Test 3: Ausgabe der Gleise vor dem Sortieren
+        System.out.println("\nTest 3: Ausgabe der Gleise vor dem Sortieren");
+        bahnhof.gleiseAusgeben();
+        // Erwartete Ausgabe: 
+        // Gleis 1: blau, rot, rot, blau, rot
+        // Gleis 2: 
+        // Gleis 3:
+
+        // Test 4: Sortieren der Waggons
+        System.out.println("\nTest 4: Sortieren der Waggons");
+        bahnhof.sortieren();
+        System.out.println("Waggons sortiert.");
+
+        // Test 5: Ausgabe der Gleise nach dem Sortieren
+        System.out.println("\nTest 5: Ausgabe der Gleise nach dem Sortieren");
+        bahnhof.gleiseAusgeben(); 
+        // Erwartete Ausgabe:
+        // Gleis 1:
+        // Gleis 2: rot, rot, rot
+        // Gleis 3: blau, blau
+
+        // --- Testfälle zum Ausfüllen ---
+
+        // Test 6: Erstellen eines neuen Bahnhofs und Befüllen mit einer anderen Reihenfolge von Waggons
+        System.out.println("\nTest 6: Erstellen eines neuen Bahnhofs und Befüllen mit einer anderen Reihenfolge von Waggons");
+        Bahnhof bahnhof2 = new Bahnhof(); 
+        // TODO: Befülle bahnhof2 mit Waggons in einer anderen Reihenfolge.
+        
+        // Ausgabe der Gleise vor dem Sortieren (bahnhof2)
+        System.out.println("Ausgabe der Gleise vor dem Sortieren (bahnhof2)");
+        bahnhof2.gleiseAusgeben(); 
+        // Erwartete Ausgabe: (abhängig von der gewählten Reihenfolge)
+        
+        // Sortieren und Ausgabe der Gleise (bahnhof2)
+        System.out.println("Sortieren und Ausgabe der Gleise (bahnhof2)");
+        bahnhof2.sortieren();
+        bahnhof2.gleiseAusgeben();
+        // Erwartete Ausgabe: (abhängig von der gewählten Reihenfolge)
+        
+        
+        // Test 7: Testen mit einem leeren Bahnhof
+        System.out.println("\nTest 7: Testen mit einem leeren Bahnhof");
+        Bahnhof bahnhof3 = new Bahnhof();
+        // TODO: Rufe die Methode sortieren() auf und gib die Gleise aus. Was passiert?
+        System.out.println("Sortieren und Ausgabe der Gleise (bahnhof3)");
+        bahnhof3.sortieren();
+        bahnhof3.gleiseAusgeben();
+        // Erwartete Ausgabe:
+    }
+}
