2c9adfbf63fce97d92dda264456fe2028209609e
Merlin <github@bothimtv.com>
2024-12-02 11:13:30 +0000

    Aufgabe 6

--- WartezimmerUI.java
+++ WartezimmerUI.java
@@ -10,31 +10,31 @@ public class WartezimmerUI {
     Patient p5 = new Patient("Menko Mansen", "DEBEKA");
 
     // Patient p1 kommt ins Wartezimmer
-    // wz.einfuegen(p1);
+    wz.einfuegen(p1);
 
     // Patient p2 kommt ins Wartezimmer
-    // wz.einfuegen(p2);
+    wz.einfuegen(p2);
 
     // Anzahl der Patienten ausgeben
-    // System.out.println(wz.anzahl());
+    System.out.println(wz.anzahl());
 
     // Patient p3 kommt ins Wartezimmer
-    // wz.einfuegen(p3);
+    wz.einfuegen(p3);
 
     // Patient p2 hat einen Termin und geht vorzeitig, danach wird die Anzahl der
     // Patienten im Wartezimmer ausgegeben
-    // System.out.println(wz.loeschen("Sabine Musterfrau"));
-    // System.out.println(wz.anzahl());
+    System.out.println(wz.loeschen("Sabine Musterfrau"));
+    System.out.println(wz.anzahl());
 
     // Der erste Patient kann ins Behandlungszimmer
-    // System.out.println(wz.naechsterPatient().getName());
+    System.out.println(wz.naechsterPatient().getName());
 
     // Der zweite Patient kann ins Behandlungszimmer
-    // System.out.println(wz.naechsterPatient().getName());
+    System.out.println(wz.naechsterPatient().getName());
 
     // Patienten p4 und p5 kommen ins Wartezimmer
-    // wz.einfuegen(p4);
-    // wz.einfuegen(p5);
+    wz.einfuegen(p4);
+    wz.einfuegen(p5);
 
   }
 }
