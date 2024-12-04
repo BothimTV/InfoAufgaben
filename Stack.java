0ec693fa7922ea8f4ae6e44b04d365ebd0dc5fa6
Merlin <github@bothimtv.com>
2024-12-04 07:01:50 +0000

    Format

--- Stack.java
+++ Stack.java
@@ -1,54 +1,46 @@
-public class Stack{
+public class Stack {
   private int[] s;
- 
-  public Stack()
-  {
+
+  public Stack() {
     s = new int[0];
-  }                  
-  
-  public boolean isEmpty()
-  {
+  }
+
+  public boolean isEmpty() {
     if (s.length == 0) {
       return true;
-    }
-    else {
+    } else {
       return false;
     } // end of if-else
   }
 
-  
-  public int top(){
+  public int top() {
     if (!isEmpty()) {
       return s[0];
-    } 
-    else {
+    } else {
       return 0;
     } // end of if-else
   }
-    
-    
-    
-  public void push(int inhalt){
-    int[] hilf = new int[s.length +1];
+
+  public void push(int inhalt) {
+    int[] hilf = new int[s.length + 1];
     for (int i = 0; i < s.length; i++) {
-      hilf[i+1] = s[i];
+      hilf[i + 1] = s[i];
     }
     hilf[0] = inhalt;
     s = hilf;
   }
-      
-      
-  public int pop(){
-    if(!isEmpty()){
+
+  public int pop() {
+    if (!isEmpty()) {
       int element = s[0];
-      int[] hilf = new int[s.length -1];
+      int[] hilf = new int[s.length - 1];
       for (int i = 0; i < hilf.length; i++) {
-        hilf[i] = s[i+1];
+        hilf[i] = s[i + 1];
       }
       s = hilf;
       return element;
     }
     return 0;
   }
-      
-} 
+
+}
