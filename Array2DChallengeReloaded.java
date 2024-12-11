f7c9ab37c8ae96f394ecfe5ec5891015df6ea5e6
Merlin <github@bothimtv.com>
2024-12-11 07:04:55 +0000

    Initial commit

---
+++ Array2DChallengeReloaded.java
@@ -0,0 +1,54 @@
+public class Array2DChallengeReloaded {
+
+    public static void main(String[] args) {
+        // Hier kannst du deine Arrays erstellen und die Methoden testen
+
+        // Beispiel:
+        int[][] testArray = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        // int[][] testArray2 = {{1,2,3,4},{4,5,6,4},{7,8,9,4},{7,8,9,4}};
+        // int[][] testArray2 = {{1,2,3},{4,5,6},{7,8,9}};
+        // int[][] testArray2 = {{1,2,3,4},{4,5,6,4},{7,8,9,4}};
+        // int[][] testArray2 = {{1,2,3},{4,5,6},{7,8,9},{7,8,9}};
+
+        ausgeben(testArray);
+    }
+
+    // Aufgabe 1.1
+    public static void ausgeben(int[][] array) {
+        // Alle Elemente ausgeben
+    }
+
+    // Aufgabe 1.2
+    public static int zaehleUngerade(int[][] array) {
+        // Code hier ergänzen
+        return 0;
+    }
+
+    // Aufgabe 2.1
+    public static int[] zeilensummen(int[][] array) {
+        // Code hier ergänzen
+        return null;
+    }
+
+    // Aufgabe 2.2
+    public static int spaltensumme(int[][] array, int spalte) {
+        // Code hier ergänzen
+        return 0;
+    }
+
+    // Aufgabe 3.1
+    public static boolean zeileEnthalten(int[][] array, int[] zeile) {
+        // Code hier ergänzen
+        return false;
+    }
+
+    // Aufgabe 3.2 (Zusatz)
+    public static int[][] multipliziere(int[][] array, int faktor) {
+        // Code hier ergänzen
+        return null;
+    }
+}
