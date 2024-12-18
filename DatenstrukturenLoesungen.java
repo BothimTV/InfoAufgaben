debcb8dd675e4cffc65c018d9ef4b4d69d514180
Merlin <github@bothimtv.com>
2024-12-18 07:03:50 +0000

    Aufgabe 1.2

--- DatenstrukturenLoesungen.java
+++ DatenstrukturenLoesungen.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class DatenstrukturenLoesungen {
 
     public static void main(String[] args) {
@@ -12,6 +10,14 @@ public class DatenstrukturenLoesungen {
 
         System.out.println("Element an Index 0: " + dynArray.getItem(0));
         System.out.println("Element an Index 2: " + dynArray.getItem(2));
+        System.out.println("Länge: " + dynArray.getLength());
+
+        dynArray.insertAt(1, 15);
+        dynArray.setItem(3, 25);
+
+        for (int i = 0; i < dynArray.getLength(); i++) {
+            System.out.println("Element an Index " + i + ": " + dynArray.getItem(i));
+        }
         System.out.println("Länge: " + dynArray.getLength());
 
         // Stack - Aufgaben
