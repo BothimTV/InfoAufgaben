ef363b07621f5cf7508d9dbf8cdc6a22edf62b10
Merlin <github@bothimtv.com>
2025-01-20 12:05:57 +0000

    Aufgabe 3

--- Main.java
+++ Main.java
@@ -32,5 +32,8 @@ public class Main {
 
         System.out.println("Breitensuche ab Knoten 4:");
         graph1.breitensuche(4);
+
+        System.out.println("Tiefensuche ab Knoten 4:");
+        graph1.starteTiefensuche(4);
     }
 }
