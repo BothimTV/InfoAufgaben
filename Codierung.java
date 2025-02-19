a6980fe6e5fef61cad0e8fd9b671506edc441e2c
Merlin <development@bothimtv.com>
2025-02-19 07:09:58 +0000

    Initial commit

---
+++ Codierung.java
@@ -0,0 +1,63 @@
+public class Codierung {
+  public static void main(String[] args) {
+    // Testen der Methoden:
+
+    System.out.println("Zeichenkette:");
+    Zeichenkette();
+
+    System.out.println("\nErsetzen:");
+    Ersetzen("ä ö ü Hallo");
+
+    // System.out.println("Passworteingabe");
+    // PasswortKorrekt("geheim123");
+
+    // System.out.println("ZiffernErsetzen:");
+    // ZiffernErsetzen("Hallo 123 Welt");
+
+    // System.out.println("\nAnzahlE:");
+    // AnzahlE("Hallo Welt");
+  }
+
+  public static void Zeichenkette() {
+    String eingabe = "Hallo, wie geht es dir?";
+    int laenge = eingabe.length();
+    char zeichen = eingabe.charAt(laenge - 1);
+    String ausgabe = "Der Text: " + eingabe + " ist " + laenge + " Zeichen lang.";
+    System.out.println(ausgabe);
+    ausgabe = "Das letzte Zeichen ist: " + zeichen;
+    System.out.println(ausgabe);
+
+    // Hier Methode erweitern nach Aufgabe 1 a) und b)
+  }
+
+  public static boolean PasswortKorrekt(String eingabe) {
+    // Implementierung von Aufgabe 1b
+    return false;
+  }
+
+  public static void Ersetzen(String eingabe) {
+    // Implementierung von Aufgabe 2
+    String ausgabe = "";
+    int laenge = eingabe.length();
+
+    for (int i = 0; i < laenge; i = i + 1) {
+      char zeichen = eingabe.charAt(i);
+
+      if (zeichen == 'ä') {
+        ausgabe = ausgabe + "ae";
+      } else {
+        ausgabe = ausgabe + zeichen;
+      }
+    }
+
+    System.out.println(ausgabe);
+  }
+
+  public static void ZiffernErsetzen(String eingabe) {
+    // Implementierung von Aufgabe 3
+  }
+
+  public static void AnzahlE(String eingabe) {
+    // Implementierung von Aufgabe 4
+  }
+}
